6eddec12579b33705aedbdafb6c4ca52ac1a950e
Lucas Mello <lucasmello@MacBook-Pro-de-user.local>
2024-03-19 23:56:25 -0300

    update validations

--- urbs/app/src/main/java/com/example/urbs/ui/signup/SignUpActivity.java
+++ urbs/app/src/main/java/com/example/urbs/ui/signup/SignUpActivity.java
@@ -3,7 +3,9 @@ package com.example.urbs.ui.signup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.MenuItem;
@@ -18,7 +20,13 @@ import com.google.i18n.phonenumbers.PhoneNumberUtil;
 
 import android.app.DatePickerDialog;
 import android.widget.DatePicker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.regex.Pattern;
 
 import android.widget.Toast;
 import com.google.i18n.phonenumbers.NumberParseException;
@@ -35,6 +43,8 @@ public class SignUpActivity extends AppCompatActivity {
     private PhoneNumberUtil phoneNumberUtil;
 
     private EditText cpfEditText;
+    private static final Pattern CPF_PATTERN = Pattern.compile("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}");
+
     private EditText passwordEditText;
     private EditText passwordConfirmEditText;
 
@@ -65,6 +75,10 @@ public class SignUpActivity extends AppCompatActivity {
         phoneNumberUtil = PhoneNumberUtil.getInstance();
 
         cpfEditText = findViewById(R.id.cpf);
+        cpfEditText.setInputType(InputType.TYPE_CLASS_NUMBER);
+
+        cpfEditText.addTextChangedListener(new CPFFormatter());
+
         passwordEditText = findViewById(R.id.password);
         passwordConfirmEditText = findViewById(R.id.confirmpassword);
 
@@ -73,50 +87,163 @@ public class SignUpActivity extends AppCompatActivity {
         signButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = nameEditText.getText().toString();
+                String birthday = converterData(birthdayEditText.getText().toString());
+                String cpf = cpfEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String phone = phoneEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String confirm = passwordConfirmEditText.getText().toString();
+
+                if (name.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Nome inválido", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 if (!onValidateEmail(v)) {
                     return;
                 }
                 if (!onValidatePhoneNumber(v)) {
                     return;
                 }
-                String name = nameEditText.getText().toString();
-                String email = emailEditText.getText().toString();
+                if (birthday.isEmpty()) {
+                    Toast.makeText(SignUpActivity.this, "Data de nascimento inválida", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+//                if (!isValidCPF(cpf)) {
+//                    Toast.makeText(SignUpActivity.this, "CPF inválido", Toast.LENGTH_SHORT).show();
+//                    return;
+//                }
+                if (password.isEmpty()){
+                    Toast.makeText(SignUpActivity.this, "Senha inválida", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!password.equals(confirm)){
+                    Toast.makeText(SignUpActivity.this, "Senha de confirmação inválida", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
-                User userFormat = new User(name,
+                User user = new User(name,
                         email,
-                        "1990-01-01",
-                        "41993456780",
-                        "password123",
-                        "34589677024");
+                        birthday,
+                        phone,
+                        password,
+                        cpf);
 
-                User user = new User("John Doe",
-                        "johndoe1@example.com",
-                        "1990-01-01",
-                        "41993456780",
-                        "password123",
-                        "34589677024");
+                apiManager.registerUser(user, new ApiManager.ApiCallback<Void>() {
+                    @Override
+                    public void onSuccess(Void result) {
+                        Toast.makeText(SignUpActivity.this, "Cadatro realizado com sucesso!", Toast.LENGTH_SHORT).show();
+                    }
 
-//                apiManager.registerUser(user, new ApiManager.ApiCallback<Void>() {
-//                    @Override
-//                    public void onSuccess(Void result) {
-//                        // Usuário registrado com sucesso
-//                        Log.d("MainActivity", "Usuário registrado com sucesso");
-//                    }
-//
-//                    @Override
-//                    public void onFailure(Throwable t) {
-//                        Log.e("MainActivity", "Falha ao registrar o usuário: " + t.getMessage());
-//                    }
-//                });
+                    @Override
+                    public void onFailure(Throwable t) {
+                        Toast.makeText(SignUpActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
+    }
+
+    public static String converterData(String dataBrasileira) {
+        // Formato da data brasileira
+        SimpleDateFormat formatoBrasileiro = new SimpleDateFormat("dd/MM/yyyy");
+
+        try {
+            // Parse da data brasileira para objeto Date
+            Date data = formatoBrasileiro.parse(dataBrasileira);
+
+            // Formato da data americana
+            SimpleDateFormat formatoAmericano = new SimpleDateFormat("yyyy-MM-dd");
+
+            // Convertendo a data para o formato americano e retornando como string
+            return formatoAmericano.format(data);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    private boolean isValidCPF(String cpf) {
+        if (cpf == null || cpf.length() != 11) {
+            return false;
+        }
+
+        // Verifica se todos os dígitos são iguais (caso de CPFs inválidos)
+        if (cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+
+        int[] numbers = new int[11];
+        for (int i = 0; i < 11; i++) {
+            numbers[i] = Integer.parseInt(cpf.substring(i, i + 1));
+        }
+
+        // Calcula o primeiro dígito verificador
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += numbers[i] * (10 - i);
+        }
+        int firstVerifier = 11 - (sum % 11);
+        if (firstVerifier >= 10) {
+            firstVerifier = 0;
+        }
+
+        if (numbers[9] != firstVerifier) {
+            return false;
+        }
+
+        // Calcula o segundo dígito verificador
+        sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += numbers[i] * (11 - i);
+        }
+        int secondVerifier = 11 - (sum % 11);
+        if (secondVerifier >= 10) {
+            secondVerifier = 0;
+        }
+
+        return numbers[10] == secondVerifier;
+    }
+
+    private class CPFFormatter implements TextWatcher {
+        private boolean isUpdating = false;
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (isUpdating) {
+                isUpdating = false;
+                return;
+            }
+
+            String str = s.toString().replaceAll("[^0-9]", "");
+
+            if (str.length() > 3 && str.length() <= 6) {
+                str = str.substring(0, 3) + "." + str.substring(3);
+            } else if (str.length() > 6 && str.length() <= 9) {
+                str = str.substring(0, 3) + "." + str.substring(3, 6) + "." + str.substring(6);
+            } else if (str.length() > 9) {
+                str = str.substring(0, 3) + "." + str.substring(3, 6) + "." + str.substring(6, 9) + "-" + str.substring(9);
+            }
+
+            isUpdating = true;
+            cpfEditText.setText(str);
+            cpfEditText.setSelection(str.length());
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
     }
 
     public boolean onValidateEmail(View view) {
         String email = emailEditText.getText().toString().trim();
 
         if (isValidEmail(email)) {
-            Toast.makeText(this, "Email válido: " + email, Toast.LENGTH_SHORT).show();
             return true;
         } else {
             Toast.makeText(this, "Email inválido", Toast.LENGTH_SHORT).show();
@@ -137,7 +264,6 @@ public class SignUpActivity extends AppCompatActivity {
 
             boolean isValid = phoneNumberUtil.isValidNumber(phoneNumberUtil.parse(phoneNumber, "BR"));
             if (isValid) {
-                Toast.makeText(this, "Número de telefone válido: " + phoneNumber, Toast.LENGTH_SHORT).show();
                 return true;
             } else {
                 Toast.makeText(this, "Número de telefone inválido", Toast.LENGTH_SHORT).show();
